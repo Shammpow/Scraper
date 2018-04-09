445005a0a2ec2a661ac2f5e610a148d05a5ebc55
Dakota Edwards <drakoden2000@gmail.com>
2018-04-09 10:03:21 -0700

    meh, this sucks

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Scraper
+
+* This program is designed to scrape the selected news source, put it into 
+
+* a database, and then populate an HTML page with said info.
+
+* It will also allow the user to leave notes on their selected articles.
--- server.js
+++ server.js
@@ -4,7 +4,6 @@ var mongoose = require("mongoose");
 var bodyParser = require("body-parser");
 var cheerio = require("cheerio");
 var request = require("request");
-
 var db = require("./models");
 
 var PORT = 3000;
@@ -21,6 +20,85 @@ mongoose.connect(MONGODB_URI, {
   useMongoClient: true
 });
 
-app.listen(PORT, function() {
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.get("/", function (req, res) {
+  res.render("newsinfo")
+  request("https://www.reddit.com/r/news/top/", function (error, response, html) {
+    var $ = cheerio.load(html);
+
+    $("p.title").each(function (i, element) {
+      var results = [];
+
+      var title = $(element).text();
+      var link = $(element).children().attr("href");
+      results.push({
+        title: title,
+        link: link
+      });
+
+      db.Article.create(results)
+        .then(function (dbArticle) {
+          console.log(dbArticle);
+        })
+        .catch(function (err) {
+          return res.json(err);
+        });
+    });
+  });
+});
+
+app.get("/articles", function (req, res) {
+  db.Article.find({})
+    .populate("note")
+    .then(function (dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function (err) {
+      res.json(err);
+    })
+});
+
+app.get("/articles/:id", function (req, res) {
+  db.Article.findOne({
+    _id: (req.params.id)
+  })
+    .populate("note")
+    .then(function (dbArticle) {
+      res.json(dbArticle)
+    })
+    .catch(function (err) {
+      res.json(err);
+    });
+});
+
+app.post("/articles/:id", function (req, res) {
+  db.Note.create(req.body)
+    .then(function (dbNote) {
+      return db.Article.findOneAndUpdate({
+        _id: (req.params.id)
+      }, { $set: { note: dbNote._id } }, { new: true });
+    })
+    .then(function (dbUser) {
+      res.json(dbUser);
+    })
+    .catch(function (err) {
+      res.json(err);
+    })
+});
+
+app.delete("articles/:id", function (req, res) {
+  db.Note.deleteOne({
+    _id: (req.params.id)})
+  .then(function(data) {
+    res.json(data)
+    .catch(function(err) {
+      res.json(err);
+    })
+  })
+})
+
+app.listen(PORT, function () {
   console.log("App running on port " + PORT + "!");
 });
